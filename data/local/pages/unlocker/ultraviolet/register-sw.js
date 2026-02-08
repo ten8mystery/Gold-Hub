2feb93e26b96dd7ffae41bf82551e8b12886063b
ten8mystery <146376897+ten8mystery@users.noreply.github.com>
2026-02-07 20:48:22 -0800

    Update register-sw.js

--- data/local/pages/unlocker/ultraviolet/register-sw.js
+++ data/local/pages/unlocker/ultraviolet/register-sw.js
@@ -1,7 +1,7 @@
 /**
  * Distributed with Ultraviolet and compatible with most configurations.
  */
-const stockSW = "/s/uv-sw.js";
+const stockSW = "uv-sw.js";
 
 /**
  * List of hostnames that are allowed to run serviceworkers on http:
