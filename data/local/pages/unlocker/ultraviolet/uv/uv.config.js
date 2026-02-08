f91c7147efd09797255d7f489c42447ddce43d79
ten8mystery <146376897+ten8mystery@users.noreply.github.com>
2026-02-07 21:36:42 -0800

    Update uv.config.js

--- data/local/pages/unlocker/ultraviolet/uv/uv.config.js
+++ data/local/pages/unlocker/ultraviolet/uv/uv.config.js
@@ -1,10 +1,12 @@
 self.__uv$config = {
     prefix: '/s/',
-    bare: '/bare/',
+    bare: 'https://bare.benroxy.com/bare/', // Backup Bare server
     encodeUrl: Ultraviolet.codec.xor.encode,
     decodeUrl: Ultraviolet.codec.xor.decode,
     handler: '/data/local/pages/unlocker/ultraviolet/uv.handler.js',
     bundle: '/data/local/pages/unlocker/ultraviolet/uv.bundle.js',
     config: '/data/local/pages/unlocker/ultraviolet/uv.config.js',
-    sw: '/uv-sw.js' 
+    sw: '/uv-sw.js',
+    // THIS IS THE WISP PART:
+    wisp: 'wss://wisp.mercurywork.shop/', 
 };
