6ffe62c5282aedefb6e03ae8bb82b79db9cfee40
ten8mystery <146376897+ten8mystery@users.noreply.github.com>
2026-02-07 20:54:00 -0800

    Update uv.config.js

--- data/local/pages/unlocker/ultraviolet/uv/uv.config.js
+++ data/local/pages/unlocker/ultraviolet/uv/uv.config.js
@@ -1,10 +1,10 @@
 self.__uv$config = {
-    prefix: '/s/', // This matches your '/s/internet/' scope
+    prefix: '/s/',
     bare: '/bare/',
     encodeUrl: Ultraviolet.codec.xor.encode,
     decodeUrl: Ultraviolet.codec.xor.decode,
     handler: '/uv/uv.handler.js',
     bundle: '/uv/uv.bundle.js',
     config: '/uv/uv.config.js',
-    sw: '/uv-sw.js', // Move this file to your main directory!
+    sw: '/uv-sw.js',
 };
